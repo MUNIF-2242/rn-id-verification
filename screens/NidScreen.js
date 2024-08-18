16d9091b05fb35ce7a825596d2570bce9bc23766
MUNIF <munif@shellbeehaken.com>
2024-08-18 15:40:58 +0600

    implement upload nid to s3 also detect face from image

--- screens/NidScreen.js
+++ screens/NidScreen.js
@@ -9,17 +9,20 @@ import {
   StyleSheet,
 } from "react-native";
 import * as ImagePicker from "expo-image-picker";
-import { FontAwesome } from "@expo/vector-icons"; // Import FontAwesome
+import { FontAwesome } from "@expo/vector-icons";
 
 const NidScreen = () => {
   const BASE_URL = process.env.EXPO_PUBLIC_BASE_URL;
   const [selfieImage, setSelfieImage] = useState(null);
   const [selfieUrl, setSelfieUrl] = useState("");
+  const [nidImage, setNidImage] = useState(null);
+  const [nidUrl, setNidUrl] = useState("");
   const [faceComparisonResult, setFaceComparisonResult] = useState("");
+  const [nidFaceComparisonResult, setNidFaceComparisonResult] = useState("");
 
   const [loading, setLoading] = useState(false);
+  const [nidLoading, setNidLoading] = useState(false);
 
-  // Function to handle picking a selfie from the gallery
   const pickSelfie = async () => {
     const permissionResult =
       await ImagePicker.requestMediaLibraryPermissionsAsync();
@@ -56,8 +59,8 @@ const NidScreen = () => {
 
       const data = await response.json();
       if (response.ok) {
-        setSelfieUrl(data.imageUrl); // Assuming the response contains the URL of the uploaded selfie
-        detectFace(data.imageUrl);
+        setSelfieUrl(data.imageUrl);
+        detectFace(data.imageUrl, setFaceComparisonResult);
         Alert.alert(
           "Selfie uploaded successfully!",
           `Selfie URL: ${data.imageUrl}`
@@ -68,35 +71,83 @@ const NidScreen = () => {
     } catch (error) {
       Alert.alert("An error occurred", error.message);
     } finally {
-      setLoading(false);
+      // Adding a small delay to ensure the loading indicator is visible briefly
+      setTimeout(() => setLoading(false), 500);
     }
   };
 
-  const detectFace = async (imageUrl) => {
-    setLoading(true);
+  const pickNid = async () => {
+    const permissionResult =
+      await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    if (!permissionResult.granted) {
+      Alert.alert("Permission to access camera is required!");
+      return;
+    }
+
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+      base64: true,
+    });
+
+    if (!result.canceled) {
+      setNidImage(result.assets[0].uri);
+      uploadNid(result.assets[0].base64);
+    }
+  };
+
+  const uploadNid = async (base64Image) => {
+    setNidLoading(true);
     try {
-      const response = await fetch(
-        `${BASE_URL}/detect-face`, // Replace with your actual API endpoint
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ imageUrl }),
-        }
-      );
+      const response = await fetch(`${BASE_URL}/upload-nid`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ image: base64Image }),
+      });
 
       const data = await response.json();
       if (response.ok) {
-        //console.log(`Face Detection Result: ${JSON.stringify(data)}`);
-        setFaceComparisonResult(data.faceDetected ? "success" : "error");
+        setNidUrl(data.imageUrl);
+        detectFace(data.imageUrl, setNidFaceComparisonResult);
+        Alert.alert("NID uploaded successfully!", `NID URL: ${data.imageUrl}`);
       } else {
-        Alert.alert("Failed to detect face", `Error: ${data.message}`);
-        setFaceComparisonResult("error");
+        Alert.alert("Failed to upload NID", `Error: ${data.message}`);
       }
     } catch (error) {
       Alert.alert("An error occurred", error.message);
-      setFaceComparisonResult("error");
+    } finally {
+      // Adding a small delay to ensure the loading indicator is visible briefly
+      setTimeout(() => setNidLoading(false), 500);
+    }
+  };
+
+  const detectFace = async (imageUrl, setComparisonResult) => {
+    setLoading(true);
+    try {
+      const response = await fetch(`${BASE_URL}/detect-face`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ imageUrl }),
+      });
+
+      const data = await response.json();
+      if (response.ok) {
+        console.log(`Face Detection Result: ${JSON.stringify(data)}`);
+        setComparisonResult(data.faceDetected ? "success" : "error");
+      } else {
+        Alert.alert("Failed to detect face", `Error: ${data.message}`);
+        setComparisonResult("error");
+      }
+    } catch (error) {
+      Alert.alert("An error occurred", error.message);
+      setComparisonResult("error");
     } finally {
       setLoading(false);
     }
@@ -104,9 +155,9 @@ const NidScreen = () => {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.label}>Profile image</Text>
+      <Text>Profile image</Text>
       <View style={styles.topSpacer} />
-      <View style={styles.profilePlaceholdeContainer}>
+      <View style={styles.profilePlaceholderContainer}>
         {loading && (
           <ActivityIndicator
             size="large"
@@ -117,10 +168,10 @@ const NidScreen = () => {
         {selfieImage ? (
           <Image
             source={{ uri: selfieImage }}
-            style={styles.profileImageStyle}
+            style={{ width: 100, height: 100 }}
           />
         ) : (
-          <View style={styles.profilePlaceholdeImageStyle}></View>
+          !loading && <View style={styles.profilePlaceholderImageStyle}></View>
         )}
       </View>
       <View style={styles.topSpacer} />
@@ -133,16 +184,57 @@ const NidScreen = () => {
         {faceComparisonResult === "success" && (
           <FontAwesome
             name="check"
-            size={16}
+            size={20}
             color="#fff"
             style={styles.icon}
           />
         )}
       </TouchableOpacity>
       {faceComparisonResult === "error" && !loading && (
-        <Text style={styles.errorMessage}>
-          Uploaded image does not contain face
-        </Text>
+        <Text style={styles.errorMessage}>Please take a face image</Text>
+      )}
+
+      <View style={styles.topSpacer} />
+
+      <Text>NID image</Text>
+      <View style={styles.topSpacer} />
+      <View style={styles.profilePlaceholderContainer}>
+        {nidLoading && (
+          <ActivityIndicator
+            size="large"
+            color="#fff"
+            style={styles.loadingIndicator}
+          />
+        )}
+        {nidImage ? (
+          <Image
+            source={{ uri: nidImage }}
+            style={{ width: 100, height: 100 }}
+          />
+        ) : (
+          !nidLoading && (
+            <View style={styles.profilePlaceholderImageStyle}></View>
+          )
+        )}
+      </View>
+      <View style={styles.topSpacer} />
+      <TouchableOpacity
+        style={styles.button}
+        onPress={pickNid}
+        disabled={nidLoading || nidFaceComparisonResult === "success"}
+      >
+        <Text style={styles.buttonText}>Take NID</Text>
+        {nidFaceComparisonResult === "success" && (
+          <FontAwesome
+            name="check"
+            size={20}
+            color="#fff"
+            style={styles.icon}
+          />
+        )}
+      </TouchableOpacity>
+      {nidFaceComparisonResult === "error" && !nidLoading && (
+        <Text style={styles.errorMessage}>Please take an NID image</Text>
       )}
     </View>
   );
@@ -155,51 +247,38 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
     paddingTop: 20,
   },
-  label: { fontWeight: "900", fontSize: 12 },
   topSpacer: { marginTop: 15 },
-  profilePlaceholdeContainer: {
+  profilePlaceholderContainer: {
     position: "relative",
     width: 100,
     height: 100,
     justifyContent: "center",
     alignItems: "center",
   },
-  profilePlaceholdeImageStyle: {
+  profilePlaceholderImageStyle: {
     width: 100,
     height: 100,
     backgroundColor: "gray",
     opacity: 0.1,
   },
-  profileImageStyle: {
-    width: 100,
-    height: 100,
-  },
   loadingIndicator: {
     position: "absolute",
-    top: "50%",
-    left: "50%",
-    marginLeft: -15,
-    marginTop: -15,
     zIndex: 1,
   },
   button: {
     width: "50%",
-    paddingVertical: 10,
-    backgroundColor: "#8e44ad",
-    alignItems: "center",
     flexDirection: "row",
-    justifyContent: "center",
+    backgroundColor: "#8e44ad",
+    padding: 10,
   },
   buttonText: {
     color: "#fff",
-    fontSize: 12,
-    textTransform: "none",
+    fontSize: 16,
   },
   icon: {
-    marginLeft: 10,
+    marginLeft: 8,
   },
   errorMessage: {
     color: "red",
-    marginTop: 10,
   },
 });
